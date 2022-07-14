221328e05465f60eb2462151cd1b56dadb42580c
tyomut <tyomut@posteo.de>
2022-07-14 09:28:26 +0200

    added data & new app

--- app.R
+++ app.R
@@ -1,28 +1,33 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Define libraries ----
 library(shiny)
+library(tidyverse)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+# Helper functions ----
+replace_space <- function(txt){
+  str_replace_all(txt,'\\s','_')
+}
+
+
+# Load Data ----
+whitewine <- read_delim('./data/winequality-white.csv',delim=';', col_names = TRUE)
+whitewine <- rename_with(whitewine, replace_space)
+whitewine$quality = as.factor(whitewine$quality)
+
+# Define UI ----
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Wine Quality"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          selectInput(
+            inputId = 'selected_var',
+            label = 'Variable:',
+            choices = names(spec(whitewine)[["cols"]]),
+          )
         ),
 
         # Show a plot of the generated distribution
@@ -32,20 +37,16 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server ----
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+        #ggplot(whitewine,aes(quality,)) + geom_bar()
+        ggplot(whitewine,aes(quality, whitewine[[input$selected_var]] )) + 
+        geom_boxplot() + 
+        labs(title = input$selected_var, x = "Quality", y = input$selected_var)
+      })
 }
 
-# Run the application 
+# Run the application ----
 shinyApp(ui = ui, server = server)
